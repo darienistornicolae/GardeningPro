21284ea545e1cef1079a6e220f3b060eb9033be1
Nicolae Darie-Nistor <memedarie@gmail.com>
2023-07-06 23:33:29 +0200

    GardenPro

---
+++ Gardening/HomeScreenView.swift
@@ -0,0 +1,20 @@
+//
+//  HomeScreenView.swift
+//  Gardening
+//
+//  Created by Darie-Nistor Nicolae on 06.07.2023.
+//
+
+import SwiftUI
+
+struct HomeScreenView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct HomeScreenView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeScreenView()
+    }
+}
---
+++ Gardening/LoginView.swift
@@ -0,0 +1,20 @@
+//
+//  LoginView.swift
+//  Gardening
+//
+//  Created by Darie-Nistor Nicolae on 06.07.2023.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct LoginView_Previews: PreviewProvider {
+    static var previews: some View {
+        LoginView()
+    }
+}
