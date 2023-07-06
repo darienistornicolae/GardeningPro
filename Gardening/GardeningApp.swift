a0746c027a32e09c8aecf22ecc84326b4747eea8
Nicolae Darie-Nistor <memedarie@gmail.com>
2023-07-06 23:31:30 +0200

    Initial Commit

---
+++ Gardening/GardeningApp.swift
@@ -0,0 +1,17 @@
+//
+//  GardeningApp.swift
+//  Gardening
+//
+//  Created by Darie-Nistor Nicolae on 06.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct GardeningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
