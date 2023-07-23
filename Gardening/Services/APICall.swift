ed1ad9f9bf53fd9b9b65cd5fe18acfbc6f26f7f4
Nicolae Darie-Nistor <memedarie@gmail.com>
2023-07-23 12:12:34 +0200

    Update APICall.swift

--- Gardening/Services/APICall.swift
+++ Gardening/Services/APICall.swift
@@ -10,5 +10,14 @@ import Combine
 
 
 class APICall: ObservableObject {
+    @Published var name: String? = nil
     
+    func getPlants(name: String) async throws {
+        guard let url = URL(string: "") else { return }
+        do {
+            try await URLSession.shared.data(from: url)
+        } catch <#pattern#> {
+            <#statements#>
+        }
+    }
 }
