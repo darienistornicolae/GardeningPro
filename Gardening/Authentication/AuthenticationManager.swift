dd11f2bf0c3c8f135a2bc07ec3c9fda2cb049bce
Nicolae Darie-Nistor <memedarie@gmail.com>
2023-08-21 23:21:30 +0200

    Update AuthenticationManager.swift

--- Gardening/Authentication/AuthenticationManager.swift
+++ Gardening/Authentication/AuthenticationManager.swift
@@ -33,7 +33,7 @@ final class AuthenticationManager: ObservableObject {
         }
     }
 
-    
+    //publiv
     //MARK: User
     
     func signIn(email: String, password: String) async throws {
